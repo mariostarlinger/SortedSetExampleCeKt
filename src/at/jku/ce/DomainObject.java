05e806d53e21b33d243c765333e63c13a9c964da
Mario <mario_starlinger@gmx.at>
2011-05-04 10:31:43 +0200

    <Tag9>Barbara: hashCode() Methode ueberschrieben

--- src/at/jku/ce/DomainObject.java
+++ src/at/jku/ce/DomainObject.java
@@ -61,4 +61,16 @@ public class DomainObject {
    		return uuid.equals(domainObj.getUuid());
 	
 }
+	
+
+
+	public int hashCode() {
+   		if (uuid != null) 
+		{
+      			return uuid.hashCode();
+   		} 
+		else {
+      			return super.hashCode();
+   		}
+	}
 }
